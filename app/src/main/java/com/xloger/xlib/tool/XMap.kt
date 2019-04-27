4cceaa3d40f709d16de15c38b2f1a4e0f4731d98
xloger <phoenix@xloger.com>
2019-04-27 15:47:10 +0800

    运单重构

--- app/src/main/java/com/xloger/xlib/tool/XMap.kt
+++ app/src/main/java/com/xloger/xlib/tool/XMap.kt
@@ -1,5 +1,6 @@
 package com.xloger.xlib.tool
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import org.jetbrains.anko.commons.BuildConfig
@@ -11,34 +12,34 @@ import org.jetbrains.anko.commons.BuildConfig
  */
 object XMap {
 
-    fun navigation(addressName: String,lat: String, lng : String) {
-        if (XNative.isInstallApp(XInit.applicationContext!!, "com.autonavi.minimap")) {
-            navigationByAmap(addressName, lat, lng)
-        } else if (XNative.isInstallApp(XInit.applicationContext!!, "com.baidu.BaiduMap")) {
-            navigationByBaidu(addressName)
+    fun navigation(context: Context, addressName: String,lat: String, lng : String) {
+        if (XNative.isInstallApp(context, "com.autonavi.minimap")) {
+            navigationByAmap(context, addressName, lat, lng)
+        } else if (XNative.isInstallApp(context, "com.baidu.BaiduMap")) {
+            navigationByBaidu(context, addressName)
         } else {
             Xlog.toast("请安装高德地图或者百度地图")
         }
     }
 
-    fun navigationByBaidu(addressName: String, mode: String = "driving") {
-        val intentString = "baidumap://map/direction?destination=${addressName}&coord_type=gcj02&mode=${mode}&src=${XTool.getSelfPacakgeName(XInit.applicationContext!!)}"
+    fun navigationByBaidu(context: Context, addressName: String, mode: String = "driving") {
+        val intentString = "baidumap://map/direction?destination=${addressName}&coord_type=gcj02&mode=${mode}&src=${XTool.getSelfPacakgeName(context)}"
         try {
             val intent = Intent()
             intent.setData(Uri.parse(intentString))
-            XInit.applicationContext?.startActivity(intent)
+            context.startActivity(intent)
         } catch (ex: Exception) {
             Xlog.toast("您没有安装百度地图")
 
         }
     }
 
-    fun navigationByAmap(addressName: String,lat: String, lng : String, mode: String = "driving") {
-        val intentString = "amapuri://route/plan/?dname=${addressName}&dlat=${lat}&lng=${lng}&dev=0&coord_type=gcj02&t=0&sourceApplication=${XTool.getSelfPacakgeName(XInit.applicationContext!!)}"
+    fun navigationByAmap(context: Context, addressName: String,lat: String, lng : String, mode: String = "driving") {
+        val intentString = "amapuri://route/plan/?dname=${addressName}&dlat=${lat}&lng=${lng}&dev=0&coord_type=gcj02&t=0&sourceApplication=${XTool.getSelfPacakgeName(context)}"
         try {
             val intent = Intent()
             intent.setData(Uri.parse(intentString))
-            XInit.applicationContext?.startActivity(intent)
+            context.startActivity(intent)
         } catch (ex: Exception) {
             Xlog.toast("您没有安装高德地图")
 
